35b0bbac7020d73a229328731a5d4bd2b8280990
aashxx <tmohamedaashir@gmail.com>
2025-02-27 21:52:56 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <main className='text-center flex items-center justify-center h-screen w-full'>
+      FinDocs AI
+    </main>
+  )
+}
+
+export default App;
