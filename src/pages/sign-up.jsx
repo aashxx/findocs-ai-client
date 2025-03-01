4becc20952850ad101c8fe278c5b49e241931615
aashxx <tmohamedaashir@gmail.com>
2025-03-02 03:42:16 +0530

    fix: login

--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -5,7 +5,7 @@ import { Input } from '@/components/ui/input';
 import { AuthContext } from '@/contexts/AuthContext';
 import { signUpValidation } from '@/lib/zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { CircleUser, Eye, EyeOff, LockKeyhole, Mail } from 'lucide-react';
+import { CircleUser, Eye, EyeOff, FileSearch, LockKeyhole, Mail } from 'lucide-react';
 import React, { useContext, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
@@ -39,85 +39,94 @@ const Signup = () => {
     }, []);
 
     return (
-        <Form {...form}>
-            { loading && <Loader />}
-            <main className='flex flex-col items-center justify-center h-screen w-full'>
-                <form onSubmit={form.handleSubmit(onSignup)} className='border px-20 py-10 rounded-lg shadow-md'>
-                    <h2 className='text-3xl text-center'>Create Account</h2>
-                    <div className='my-2'>
-                        <FormDescription className='text-center'>
-                            Create an account to utilize our services
-                        </FormDescription>
-                    </div>
-                    <hr className='mb-7' />
-                    <FormField
-                        control={form.control}
-                        name='fullName'
-                        render={({ field }) => (
-                            <FormItem>
-                                <label className='text-[12px]' htmlFor="email">Full Name</label>
-                                <FormControl>
-                                    <article className="relative group">
-                                        <CircleUser strokeWidth={1} className="text-gray-500 absolute top-[6px] left-2 group-focus-within:text-black transition-colors" />
-                                        <Input className="pl-10 focus:ring-1 focus:ring-black" name="fullName" type="text" placeholder="Your Full Name" {...field} />
-                                    </article>
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name='email'
-                        render={({ field }) => (
-                            <FormItem>
-                                <label className='text-[12px]' htmlFor="email">Email Address</label>
-                                <FormControl>
-                                    <article className="relative group">
-                                        <Mail strokeWidth={1} className="text-gray-500 absolute top-[6px] left-2 group-focus-within:text-black transition-colors" />
-                                        <Input className="pl-10 focus:ring-1 focus:ring-black" name="email" type="email" placeholder="Your Email" {...field} />
-                                    </article>
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name='password'
-                        render={({ field }) => (
-                            <FormItem>
-                                <label className='text-[12px]' htmlFor="password">Password</label>
-                                <div className='relative'>
+        <main className='h-screen w-full flex items-end justify-around'>
+            <section className='flex flex-col gap-3 pb-24'>
+                <FileSearch className='h-[100px] w-[100px]' />
+                <h2 className='text-7xl'>
+                    FinDocx AI
+                </h2>
+                <p className='text-[gray]'>
+                    The Future of AI-Driven Financial Document Intelligence.
+                </p>
+            </section>
+            <Form {...form}>
+                { loading && <Loader />}
+                    <form onSubmit={form.handleSubmit(onSignup)} className='border mb-24 w-[400px] p-10 rounded-lg shadow-md'>
+                        <h2 className='text-3xl text-center'>Create Account</h2>
+                        <div className='my-2'>
+                            <FormDescription className='text-center'>
+                                Create an account to utilize our services
+                            </FormDescription>
+                        </div>
+                        <hr className='mb-7' />
+                        <FormField
+                            control={form.control}
+                            name='fullName'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <label className='text-[12px]' htmlFor="email">Full Name</label>
                                     <FormControl>
                                         <article className="relative group">
-                                            <LockKeyhole strokeWidth={1} size={19} className="text-gray-500 absolute top-[8px] left-2 group-focus-within:text-black transition-colors" />
-                                            <Input className="pl-10 focus:ring-1 focus:ring-black" type={viewPassword ? 'text' : 'password'} placeholder='Your Password' {...field} />
+                                            <CircleUser strokeWidth={1} className="text-gray-500 absolute top-[6px] left-2 group-focus-within:text-black transition-colors" />
+                                            <Input className="pl-10 focus:ring-1 focus:ring-black" name="fullName" type="text" placeholder="Your Full Name" {...field} />
                                         </article>
                                     </FormControl>
-                                    <div className='absolute right-3 cursor-pointer top-2 hover:bg-white' onClick={(e) => { e.stopPropagation(); setViewPassword(!viewPassword) }}>
-                                        {
-                                            viewPassword ? (
-                                                <Eye size={20} />
-                                            ) : (
-                                                <EyeOff size={20} />
-                                            )
-                                        }
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='email'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <label className='text-[12px]' htmlFor="email">Email Address</label>
+                                    <FormControl>
+                                        <article className="relative group">
+                                            <Mail strokeWidth={1} className="text-gray-500 absolute top-[6px] left-2 group-focus-within:text-black transition-colors" />
+                                            <Input className="pl-10 focus:ring-1 focus:ring-black" name="email" type="email" placeholder="Your Email" {...field} />
+                                        </article>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name='password'
+                            render={({ field }) => (
+                                <FormItem>
+                                    <label className='text-[12px]' htmlFor="password">Password</label>
+                                    <div className='relative'>
+                                        <FormControl>
+                                            <article className="relative group">
+                                                <LockKeyhole strokeWidth={1} size={19} className="text-gray-500 absolute top-[8px] left-2 group-focus-within:text-black transition-colors" />
+                                                <Input className="pl-10 focus:ring-1 focus:ring-black" type={viewPassword ? 'text' : 'password'} placeholder='Your Password' {...field} />
+                                            </article>
+                                        </FormControl>
+                                        <div className='absolute right-3 cursor-pointer top-2 hover:bg-white' onClick={(e) => { e.stopPropagation(); setViewPassword(!viewPassword) }}>
+                                            {
+                                                viewPassword ? (
+                                                    <Eye size={20} />
+                                                ) : (
+                                                    <EyeOff size={20} />
+                                                )
+                                            }
+                                        </div>
                                     </div>
-                                </div>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <Button type='submit' className='mt-8 w-full text-md font-semibold'>
-                        Create Account
-                    </Button>
-                    <h4 className='my-4 text-sm text-center'>
-                        Already have an account? <Link className='hover:underline' to={'/login'}>Login</Link>
-                    </h4>
-                </form>
-            </main>
-        </Form>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type='submit' className='mt-8 w-full text-md font-semibold'>
+                            Create Account
+                        </Button>
+                        <h4 className='my-4 text-sm text-center'>
+                            Already have an account? <Link className='hover:underline' to={'/login'}>Login</Link>
+                        </h4>
+                    </form>
+            </Form>
+        </main>
     )
 }
 
